75c552754666ed8d64b09ce73a5534257c1cc66c
yuchia <b07505016@ntu.edu.tw>
2022-06-01 01:00:53 +0800

    ver. 2

--- DeliveryMan/Orders.java
+++ DeliveryMan/Orders.java
@@ -1,20 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * 
- * 訂單Class
- * @author 陳德安
- *
+ * @author 許祐嘉, deanchenn
+ * @version 2022/5/29 ver.2
+ * @changes added: none <br>
+ *          changed: <br>
+ *          1. 將Address type 改成Position以符合class {@code API} 裡面 method
+ *          {@code toGlobal()} 要求.<br>
+ *          2. 將{@code orderTime}跟{@code deliverTime} 資料型態改成int以符合class
+ *          {@code API}裡面{@code travelTime}的回傳值，後面getter跟setter也改了<br>
+ * 			
  */
 public class Orders {
 	private int number;// 訂單編號
-	private Items[] items;// 餐點
+	private Queue<Items> itemList;// 餐點
 	
 	private state currentState;// 訂單狀態
 	
 	private int amount;// 訂單金額
-	private String orderTime;// 下單時間
-	private String deliverTime;// 送達時間
-	private String deliverAddress;// 配送位置
+	private int orderTime;// 下單時間
+	private int deliverTime;// 送達時間
+	private Position deliverAddress;// 配送位置
+	private Position storeAddress;// 店家位置
 	private String customerName;// 消費者姓名
 	private String deliveryManName;// 外送員名字
 	private String storeName;// 店家名字
@@ -31,11 +40,14 @@ public class Orders {
 	 * @param deliveryManName - 外送員名字
 	 * @param store           - 店家資訊
 	 */
-	public Orders(int number, Items items[], String orderTime, String deliverTime, String deliverAddress,
-			String customerName, String deliveryManName, Stores store) {
+	public Orders(int number, Queue<Items> itemList, int orderTime, int deliverTime, Position deliverAddress,
+			Position storeAddress, String customerName, String deliveryManName, Stores store) {
 		this.number = number;
+		this.itemList = itemList;
 		this.orderTime = orderTime;
+		this.deliverTime = deliverTime;
 		this.deliverAddress = deliverAddress;
+		this.storeAddress = storeAddress;
 		this.customerName = customerName;
 		this.deliveryManName = deliveryManName;
 		this.storeName = store.getName();
@@ -50,15 +62,12 @@ public class Orders {
 		this.number = number;
 	}
 	
-	public Items[] getItems() {
-		return items;
+	public Queue<Items> getItemList() {
+		return itemList;
 	}
 	
-	public void setItems(Items items[]) {
-		this.items = new Items[items.length];
-		for (int i = 0; i < items.length; i++) {
-			this.items[i] = items[i];
-		}
+	public void setItems(Queue<Items> itemList) {
+		this.itemList = new LinkedList<Items>(itemList);
 	}
 	
 	public String getCurrentState() {
@@ -77,28 +86,36 @@ public class Orders {
 		this.amount = amount;
 	}
 	
-	public String getOrderTime() {
+	public int getOrderTime() {
 		return orderTime;
 	}
 	
-	public void setOrderTime(String orderTime) {
+	public void setOrderTime(int orderTime) {
 		this.orderTime = orderTime;
 	}
 	
-	public String getDeliverTime() {
+	public int getDeliverTime() {
 		return deliverTime;
 	}
 	
-	public void setDeliverTime(String deliverTime) {
+	public void setDeliverTime(int deliverTime) {
 		this.deliverTime = deliverTime;
 	}
 	
-	public String getDeliverAddress() {
+	public Position getDeliverAddress() {
 		return deliverAddress;
 	}
 	
-	public void setDeliverAddress(String deliverAddress) {
+	public void setDeliverAddress(Position deliverAddress) {
 		this.deliverAddress = deliverAddress;
+	}
+	
+	public Position getStoreAddress() {
+		return storeAddress;
+	}
+	
+	public void setStoreAddress(Position storeAddress) {
+		this.storeAddress = storeAddress;
 	}
 	
 	public String getCustomerName() {
